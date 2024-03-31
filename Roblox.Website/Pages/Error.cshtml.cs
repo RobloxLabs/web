95205e3aacae81833ccc9728c3cc9f910af90cde
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2024-03-30 23:35:35 -0500

    Fix error page status code handling

--- Roblox.Website/Pages/Error.cshtml.cs
+++ Roblox.Website/Pages/Error.cshtml.cs
@@ -25,32 +25,48 @@ namespace Roblox.Website.Pages
 
         public string? ErrorMessage { get; set; }
 
+        public int ResponseCode { get; set; }
+
 
         public ErrorModel(ILogger<ErrorModel> logger, IStringLocalizer<MessagesResources> resources)
         {
             _logger = logger;
             Resources = resources;
+
+            // If shit really hits the fan and we can't translate, we can always fallback to this
+            ResponseCode = 500;
+            ErrorTitle = "Unexpected error with your request";
+            ErrorDescription = "Please try again after a few moments.";
         }
 
-        private void SetErrorInfo(HttpStatusCode statusCode, Exception? exception)
+        private void SetErrorInfo(HttpStatusCode? statusCode, Exception? exception = null)
         {
             switch (statusCode)
             {
+                case HttpStatusCode.BadRequest:
+                    ResponseCode = 400;
+                    ErrorTitle = Resources["Response.BadRequest"];
+                    ErrorDescription = Resources["Response.BadRequestDescription"];
+                    break;
                 case HttpStatusCode.Forbidden:
+                    ResponseCode = 403;
                     ErrorTitle = Resources["Response.AccessDenied"];
                     ErrorDescription = Resources["Response.AccessDeniedDescription"];
                     break;
                 case HttpStatusCode.NotFound:
+                    ResponseCode = 404;
                     ErrorTitle = Resources["Response.PageNotFound"];
                     ErrorDescription = Resources["Response.PageNotFoundDescription"];
                     break;
                 case HttpStatusCode.InternalServerError:
+                    ResponseCode = 500;
                     ErrorTitle = Resources["Response.InternalServerError"];
                     ErrorDescription = Resources["Response.InternalServerErrorDescription"];
                     break;
-                default: // Covers HttpStatusCode.BadRequest & anything else the user might throw at us
-                    ErrorTitle = Resources["Response.BadRequest"];
-                    ErrorDescription = Resources["Response.BadRequestDescription"];
+                default: // Covers any other error codes the user might throw at us
+                    ResponseCode = 500;
+                    ErrorTitle = Resources["Response.SomethingWentWrong"];
+                    ErrorDescription = Resources["Response.UnexpectedError"];
                     break;
             }
 
@@ -60,29 +76,40 @@ namespace Roblox.Website.Pages
 
         private IActionResult OnRequest()
         {
-            // HACK: Clear HttpContext items to prevent script and CSS files from bleeding over from errored page
-            HttpContext.Items.Clear();
-
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-            var codeStr = Request.Query["code"].FirstOrDefault();
-            HttpStatusCode code;
-            if (!Enum.TryParse(codeStr, out code))
-                code = HttpStatusCode.BadRequest;
-            HttpContext.Response.StatusCode = (int)code;
-
-            Exception? exception = null;
-            var feature =
-                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            if (feature != null && feature.Error != null)
+            try
             {
-                exception = feature.Error;
-                // All exceptions give 403s by default
-                code = HttpStatusCode.InternalServerError;
+                // HACK: Clear HttpContext items to prevent script and CSS files from bleeding over from errored page
+                HttpContext.Items.Clear();
+
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+                var codeStr = Request.Query["code"].FirstOrDefault();
+                HttpStatusCode? statusCode = null;
+                if (Enum.TryParse<HttpStatusCode>(codeStr, out var codeOut))
+                    statusCode = codeOut;
+
+                Exception? exception = null;
+                var feature =
+                    HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+                if (feature != null && feature.Error != null)
+                {
+                    exception = feature.Error;
+                    // All exceptions give 500s by default
+                    statusCode = HttpStatusCode.InternalServerError;
+                }
+
+                SetErrorInfo(statusCode, exception);
+                HttpContext.Response.StatusCode = ResponseCode;
             }
+            catch { }
 
-            SetErrorInfo(code, exception);
+            try
+            {
+                return Page();
+            }
+            catch { }
 
-            return Page();
+            // If everything's imploded, fallback to plaintext
+            return Content("The site is currently offline for maintenance and upgrades.\r\nPlease check back soon!");
         }
 
         public IActionResult OnGet() => OnRequest();
